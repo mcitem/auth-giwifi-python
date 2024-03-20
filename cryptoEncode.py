c3c5a7d851c1edb4bc8675ae99f817a8f4e69f33
mcitem <mcitem@mcitem.com>
2024-03-20 22:35:46 +0800

    update cryptoEncode.py

--- cryptoEncode.py
+++ cryptoEncode.py
@@ -1,14 +1,10 @@
 from Crypto.Cipher import AES
 import base64
-def zero_pad(data, block_size=AES.block_size):
-    pad_length = block_size - (len(data) % block_size)
-    return data + bytes([0] * pad_length)
 def cryptoEncode(oriData,iv):
     oriData = oriData.encode('utf-8')
-    iv = iv.encode('utf-8')
     key=b'1234567887654321'
-    aes =AES.new(key,AES.MODE_CBC,iv)
-    padData = zero_pad(oriData)
-    encrypted = aes.encrypt(padData)
+    aes =AES.new(key,AES.MODE_CBC,iv.encode('utf-8'))
+    oriData+=bytes([0]*(AES.block_size-(len(oriData)%AES.block_size)))
+    encrypted = aes.encrypt(oriData)
     cryptoData = base64.b64encode(encrypted)
     return cryptoData.decode('utf-8')
